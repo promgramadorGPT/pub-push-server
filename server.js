5d99656116f92709e51baa60107b29e81bca29b4
promgramadorGPT <cinemacinefilo@gmail.com>
2026-07-08 18:31:27 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
+const express = require("express");
 
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Servidor Push Online 🚀");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
