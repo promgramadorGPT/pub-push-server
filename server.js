65f91dae0aee50568e87a865c51b87ed348a099e
promgramadorGPT <cinemacinefilo@gmail.com>
2026-07-12 01:16:19 -0300

    Update server.js

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ async function enviarPush(numero){
                         requireInteraction: true,
                         vibrate: [500, 300, 500, 300],
                         icon: "https://kaospub2.netlify.app/senha/icon-512.png"
-                        badge: "https://cdn-icons-png.flaticon.com/512/3075/3075977.png"
+                        badge: "https://kaospub2.netlify.app/senha/icon-512.png"
                     },
                     // 🔥 2. AQUI ADICIONAMOS A AÇÃO DE ABRIR O SITE AO CLICAR
                     fcmOptions: {
